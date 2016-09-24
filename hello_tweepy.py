745a65382145cbc9d7e8b00c97e69f9bb6f35fcd
Heguang Miao <hikuimiao@gmail.com>
2016-09-24 19:00:00 +1000

    stream

--- hello_tweepy.py
+++ hello_tweepy.py
@@ -2,26 +2,36 @@ import sys
 import tweepy
 import codecs
 import string
+import json
 
 auth = tweepy.OAuthHandler("3779qAQHaXeCoez6gbePLPgx8", "Smh4geVG3LusJBLRBJoI1Bucj5NUDvEJkYGG0d8Y9dvdsI521n")
 auth.set_access_token("110147932-RQMjJCMxN26FrIh4lPBFM88HhDrrGLUWdD868RO7", "DLn6oxudmGHJoE1OV6WS9kj0pBVsSxqxX9xgQpyE8ysM3")
 
-api = tweepy.API(auth)
+from pymongo import MongoClient
 
-filepath = './tweet.txt'
 
-def search_tweets(query, count=200):
-    search_results = api.search(q=query, count=count, lang="en")
-    for tweet in search_results:
-        write_tweets(filepath, tweet)
+class MongoStreamListener(tweepy.StreamListener):
 
-def write_tweets(filename, tweet):
-    with codecs.open(filename, 'a', 'utf-8') as f:
-        f.write(string.replace(tweet.text, '\n', ''))
-        f.write('\n')
+    def __init__(self):
+        super(MongoStreamListener, self).__init__()
+        client = MongoClient('115.146.88.96',27017)
+        db = client.final_project
+        collection = db.tweets
+        self.collection = collection
+
+    def on_status(self, status):
+        json_str = json.dumps(status._json)
+        obj = json.loads(json_str)
+        inserted_id = self.collection.insert_one(obj).inserted_id
+        print(inserted_id)
+        
+
+
 
 def main():
-    search_tweets('iphone7')
+    twitter_stream_listener = MongoStreamListener()
+    stream = tweepy.Stream(auth, twitter_stream_listener)
+    stream.sample(languages=['en'])
 
 if __name__ == '__main__':
     sys.exit(int(main() or 0))
